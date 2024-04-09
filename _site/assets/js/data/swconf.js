376c3566bacac2244d23b7a80717c75e8ee7a79e
jingnafeng <163872795+jingnafeng@users.noreply.github.com>
2024-04-09 17:17:58 -0500

    static sites built

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712699612',resources: [
+    cacheName: 'chirpy-1712700966',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
