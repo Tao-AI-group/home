3e8f030f6a25cdc4105b18bdfcd8db1be7f9f618
jingnafeng <163872795+jingnafeng@users.noreply.github.com>
2024-04-09 14:02:16 -0500

    Add files via upload
    first sites

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,45 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1712689173',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/team/',
+      
+        '/projects/',
+      
+        '/categories/',
+      
+        '/publications/',
+      
+        '/archives/',
+      
+        '/tags/',
+      
+        '/about/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],allowHosts: [
+      
+        'TaoAI.github.io',
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
